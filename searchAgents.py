4f30856e75cbb4662cf7bcbc3969b0240bc0fe49
juliacu <65301609+juliacu@users.noreply.github.com>
2022-05-15 21:56:39 +0200

    Update searchAgents.py

--- searchAgents.py
+++ searchAgents.py
@@ -288,6 +288,7 @@ class CornersProblem(search.SearchProblem):
         # Please add any code here which you would like to use
         # in initializing the problem
         "*** YOUR CODE HERE ***"
+        self.initSt = [0, 0, 0, 0]
 
     def getStartState(self):
         """
@@ -295,14 +296,20 @@ class CornersProblem(search.SearchProblem):
         space)
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        #util.raiseNotDefined()
+        return (self.startingPosition, self.initSt)
 
     def isGoalState(self, state):
         """
         Returns whether this search state is a goal state of the problem.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        #util.raiseNotDefined()
+        for item in state[1]:
+            if item == 0:
+                return False
+            
+        return True
 
     def getSuccessors(self, state):
         """
@@ -325,6 +332,21 @@ class CornersProblem(search.SearchProblem):
             #   hitsWall = self.walls[nextx][nexty]
 
             "*** YOUR CODE HERE ***"
+            x, y = state[0]
+            corner = state[1][:]
+            
+            dx, dy = Actions.directionToVector(action)
+            nextx, nexty = int(x + dx), int(y + dy)
+            
+            if not self.walls[nextx][nexty]:
+                
+                if (nextx, nexty) in self.corners:
+                    corner[self.corners.index((nextx, nexty))] = 1
+                    
+                nextState = ((nextx, nexty), corner)
+                cost = 1
+                
+                successors.append((nextState, action, cost))
 
         self._expanded += 1 # DO NOT CHANGE
         return successors
